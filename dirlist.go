42f0c9ae0c09fad5ceb1c539f5357994696abea3
Rob8150 <robert.clark.1967@gmail.com>
2022-08-12 18:54:29 +0930

    Go program to list files in directory

---
+++ dirlist.go
@@ -0,0 +1,10 @@
+package main
+
+//cd C:\Users\rober\go
+//go run src\github.com\Rob8150\dirlist\directory.go
+
+func main() {
+	search := "github.com/Rob8150"
+	match := ".go"
+	dirlist(search, match)
+}
